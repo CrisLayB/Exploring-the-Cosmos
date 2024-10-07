22624217e91699d94a784df887fc8490be188866
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2024-10-06 21:52:15 -0600

    Pause Menu and Collectionables Menu Ready.

---
+++ ExploringTheCosmos-TheGame/Assets/Scripts/ShowMenus.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenus : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuGO;
+    private PauseMenu pauseMenu;
+
+    [SerializeField] private GameObject logMenuGO;
+    private LogMenu logMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuGO.SetActive(false);        
+        logMenuGO.SetActive(false);
+
+        pauseMenu = pauseMenuGO.GetComponent<PauseMenu>();
+        logMenu = logMenuGO.GetComponent<LogMenu>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("PauseMenu"))
+        {            
+            if (pauseMenuGO.activeSelf) 
+            {
+                StartCoroutine(pauseMenu.PopDownMenu());
+                if(GameManager.Instance != null) GameManager.Instance.LockMouseCursor();
+                if(GameManager.Instance != null) GameManager.Instance.ContinuePlaying();
+            } 
+            else 
+            {
+                if(logMenuGO.activeSelf) return;
+                
+                pauseMenuGO.SetActive(true);
+                StartCoroutine(pauseMenu.PopUpMenu());
+                if(GameManager.Instance != null) GameManager.Instance.UnlockMouseCursor();
+                if(GameManager.Instance != null) GameManager.Instance.StopPlaying();
+            }
+        }
+
+        if(Input.GetButtonDown("CollectionMenu"))
+        {
+            if (logMenuGO.activeSelf) 
+            {
+                StartCoroutine(logMenu.PopDownMenu());
+                if(GameManager.Instance != null) GameManager.Instance.LockMouseCursor();
+                if(GameManager.Instance != null) GameManager.Instance.ContinuePlaying();
+            } 
+            else 
+            {
+                if (pauseMenuGO.activeSelf) return;
+
+                logMenuGO.SetActive(true);
+                StartCoroutine(logMenu.PopUpMenu());
+                if(GameManager.Instance != null) GameManager.Instance.UnlockMouseCursor();
+                if(GameManager.Instance != null) GameManager.Instance.StopPlaying();
+            }
+        }
+    }
+}
